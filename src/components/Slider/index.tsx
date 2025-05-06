6dfc4e17f9c00287d5cf6f202c2000b3b7b5a63f
Tryd0g0lik <work80@mail.ru>
2025-05-06 09:04:31 +0700

    [master]:scss was updated

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -7,11 +7,27 @@ import "src/components/Slider/styles/style.scss";
 export function SliderFC(): JSX.Element {
   return (
     <div className="slider">
-        <div className="slider__track">
-            <div className="slider__fill">
-              
-            </div>
+      <section className="slider__track">
+        <div className="h2 slider_name">
+          <h2>Хиты продаж</h2>
         </div>
+
+        <div className="slider_icons__fill">
+          <div className="slider_icons__fill-inner icons">
+            <div className="icons">
+              <div className="icon"></div>
+              <div className="icon"></div>
+              <div className="icon"></div>
+              <div className="icon"></div>
+              <div className="icon"></div>
+            </div>
+          </div>
+          <div className="slider_icons__fill-inner icon2"></div>
+          <div className="slider_icons__fill-inner icon3"></div>
+          <div className="slider_icons__fill-inner icon4"></div>
+          <div className="slider_icons__fill-inner icon5"></div>
+        </div>
+      </section>
     </div>
     );
 }
