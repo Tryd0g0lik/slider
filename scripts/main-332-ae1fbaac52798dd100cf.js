dd50653293942d4c971da63883890c687765b2f3
Tryd0g0lik <Tryd0g0lik@users.noreply.github.com>
2025-07-02 02:00:11 +0000

    deploy: a567f02cb1a61dceddf8aea062a108ae26d09e8c

--- scripts/main-332-ae1fbaac52798dd100cf.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkslider=self.webpackChunkslider||[]).push([[332],{814:(e,s,l)=>{var r=l(543),n=l.n(r);console.log(n().join(["Another","module","loaded!"]," "))}},e=>{e.O(0,[804],(()=>{return s=814,e(e.s=s);var s}));e.O()}]);
